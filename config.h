4c865a50868b21dbdc070548f500d7ba9d58b11e
ashp8 <ashis@techzu.co>
2023-07-12 18:45:35 +0600

    config updates

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;     /* border pixel of windows */
 static const unsigned int snap      = 0;       /* snap pixel */
 static const unsigned int gappx     = 5;       /* gap pixel */
 static const int showbar            = 5;     /* 0 means no bar */
-static const int topbar             = 2;        /* 0 means bottom bar */
+static const int topbar             = 3;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Source Code Pro:size=11" };
 static const char dmenufont[]       = "Source Code Pro:size=11";
 
